4b09a3625940cccf7dd9c97d61f6230300473596
aamarillas <aamarillas@marqeta.com>
2020-01-21 15:56:44 -0800

    LRU cache

--- src/main/java/adan/amarillas/algorithms/LRUCache.java
+++ src/main/java/adan/amarillas/algorithms/LRUCache.java
@@ -2,36 +2,30 @@ package adan.amarillas.algorithms;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Objects;
 
-public class LRUCache {
-  int capacity;
-  MyLinkedHashMap<Integer, Integer> cache = new MyLinkedHashMap();
+public class LRUCache extends LinkedHashMap<Integer, Integer> {
+
+  private int capacity;
 
   public LRUCache(int capacity) {
+    super(capacity, 1.1f, true); // load factor = 1.1 so that underlying array is never resized/increased.
     this.capacity = capacity;
   }
 
-  //  get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+  // get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
   // O(1) time complexity since we're using LinkedHashMap.
   public int get(int key) {
-    return Objects.isNull(cache.containsKey(key)) ? cache.get(key) : -1;
+    return containsKey(key) ? get(key) : -1;
   }
 
-  //  put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity,
-  //  it should invalidate the least recently used item before inserting a new item.
+  // put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity,
+  // it should invalidate the least recently used item before inserting a new item.
   public void put(int key, int value) {
-    if (Objects.nonNull(cache.containsKey(key))) {
-      cache.replace(key, value);
-    } else {
-      cache.put(key, value);
-    }
+    put(Integer.valueOf(key), Integer.valueOf(value));
   }
 
-  private final class MyLinkedHashMap<K, V> extends LinkedHashMap<K, V> {
-    @Override
-    protected boolean removeEldestEntry(Map.Entry eldest) {
-     return size() > capacity;
-    }
+  @Override
+  protected boolean removeEldestEntry(Map.Entry eldest) {
+    return size() > capacity;
   }
 }
